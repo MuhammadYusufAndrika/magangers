59247e502e6839ecd0fe4ecec0c200d3de86077c
MuhammadYusufAndrika <andrika00748@gmail.com>
2025-10-01 11:10:30 +0700

    update profile

--- app/(landing)/_components/hero.tsx
+++ app/(landing)/_components/hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div className="hero">
       <motion.div initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1, delay: 0.2 }} className="font-dmserif text-brown-700 text-5xl md:text-6xl text-center mb-10 uppercase text-bold">
-       Magang Dahana 2025
+       Ikatan Magangers Dahana
       </motion.div>
       <motion.div initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1, delay: 0.5 }}>
         <Image className="w-full rounded-xl aspect-[21/9] max-sm:aspect-video object-cover" src="/images/herocoverr.jpg" width={2000} height={1000} alt="" />
